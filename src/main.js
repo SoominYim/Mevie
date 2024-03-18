134789a212dd0f68e793eae71ceb081524fe47ed
SoominYim <soo15soo@kakao.com>
2024-03-18 18:18:27 +0900

    feat : Add global axios configuration

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
 
-createApp(App).use(router).use(store).mount("#app");
+const app = createApp(App);
+app.config.globalProperties.$axios = axios;
+app.use(router).use(store).mount("#app");
