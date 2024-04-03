b69c257f06869ca19c6ccf34ab20474505de7cd5
SoominYim <soo15soo@kakao.com>
2024-04-03 13:31:53 +0900

    feat : swiper libary 추가

--- src/components/mobile/MoGenreMovie.vue
+++ src/components/mobile/MoGenreMovie.vue
@@ -2,34 +2,39 @@
   <section class="genre_movie" ref="section">
     <h2>{{ this.kor }}</h2>
     <div v-show="loading" class="loading-overlay"></div>
-    <div
-      v-show="!loading"
-      ref="genre__slider"
-      @touchstart="handleTouchStart"
-      @touchmove="handleTouchMove"
-      @touchend="handleTouchEnd"
-      @touchcancel="handleTouchCancel"
+    <swiper
+      :slidesPerView="4.2"
+      :spaceBetween="10"
+      :freeMode="true"
+      :pagination="{
+        clickable: true,
+      }"
+      :modules="modules"
       class="genre-slider-wrap"
     >
-      <ul class="genre__slider">
-        <li class="genre__item" v-for="(item, i) in genreItems" :class="`item${i + 1}`" :key="i">
-          <router-link :to="{ path: `/detail/${item.id}` }">
-            <div class="genre__thumbnail">
-              <img :src="item.poster" alt="배경사진" :style="{ width: `${80}px`, height: `${120}px` }" />
-            </div>
-            <p class="genre__title blind">{{ item.title }}</p>
-          </router-link>
-        </li>
-      </ul>
-    </div>
+      <swiper-slide v-for="(item, i) in genreItems" :class="`item${i + 1}`" :key="i">
+        <router-link :to="{ path: `/detail/${item.id}` }">
+          <div class="genre__thumbnail">
+            <img :src="item.poster" alt="배경사진" :style="{ width: `${80}px`, height: `${120}px` }" />
+          </div>
+          <p class="genre__title blind">{{ item.title }}</p>
+        </router-link>
+      </swiper-slide>
+    </swiper>
   </section>
 </template>
 
 <script>
 import { mapState } from "vuex";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { FreeMode } from "swiper/modules";
+// Import Swiper styles
+import "swiper/css";
+
+import "swiper/css/free-mode";
 export default {
   name: "MoGenreMovie",
-  components: {},
+  components: { Swiper, SwiperSlide },
   props: ["no", "kor", "eng"],
   data() {
     return {
@@ -49,7 +54,11 @@ export default {
     ...mapState(["url", "params", "imgURL"]),
   },
   created() {},
-
+  setup() {
+    return {
+      modules: [FreeMode],
+    };
+  },
   methods: {
     getMovie(No) {
       this.loading = true;
@@ -80,44 +89,44 @@ export default {
           this.loading = false;
         });
     },
-    handleTouchStart(event) {
-      window.addEventListener(
-        "touchstart",
-        function () {
-          event.preventDefault();
-        },
-        { passive: false }
-      );
-      this.isDragging = true;
-      this.startX = event.touches[0].clientX;
-      this.startTranslate = this.currentTranslate; // 터치 시작 시 이전 translate 값 저장
-    },
-    handleTouchMove(event) {
-      window.addEventListener(
-        "touchmove",
-        function () {
-          event.preventDefault();
-        },
-        { passive: false }
-      );
+    // handleTouchStart(event) {
+    //   window.addEventListener(
+    //     "touchstart",
+    //     function () {
+    //       event.preventDefault();
+    //     },
+    //     { passive: false }
+    //   );
+    //   this.isDragging = true;
+    //   this.startX = event.touches[0].clientX;
+    //   this.startTranslate = this.currentTranslate; // 터치 시작 시 이전 translate 값 저장
+    // },
+    // handleTouchMove(event) {
+    //   window.addEventListener(
+    //     "touchmove",
+    //     function () {
+    //       event.preventDefault();
+    //     },
+    //     { passive: false }
+    //   );
 
-      if (!this.isDragging) return;
+    //   if (!this.isDragging) return;
 
-      const currentTouchX = event.touches[0].clientX;
-      this.diff = this.startX - currentTouchX;
-      this.currentTranslate = this.currentTranslate = Math.max(
-        window.innerWidth - 1715,
-        Math.min(0, this.startTranslate - this.diff)
-      ); // 이동량에 따라 translate 값 변경
-      this.updateSliderPosition();
-    },
-    handleTouchEnd() {
-      this.isDragging = false;
-    },
-    updateSliderPosition() {
-      const slider = this.$refs.genre__slider;
-      slider.style.left = `${this.currentTranslate}px`;
-    },
+    //   const currentTouchX = event.touches[0].clientX;
+    //   this.diff = this.startX - currentTouchX;
+    //   this.currentTranslate = this.currentTranslate = Math.max(
+    //     window.innerWidth - 1715,
+    //     Math.min(0, this.startTranslate - this.diff)
+    //   ); // 이동량에 따라 translate 값 변경
+    //   this.updateSliderPosition();
+    // },
+    // handleTouchEnd() {
+    //   this.isDragging = false;
+    // },
+    // updateSliderPosition() {
+    //   const slider = this.$refs.genre__slider;
+    //   slider.style.left = `${this.currentTranslate}px`;
+    // },
   },
   mounted() {
     this.getMovie(this.no);
